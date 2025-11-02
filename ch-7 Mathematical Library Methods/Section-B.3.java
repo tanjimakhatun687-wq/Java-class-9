24be6cbbaf3d2d159b8a2952e08ac74275ae3328
tanjimakhatun687-wq <tanjimakhatun687@gmail.com>
2025-11-02 04:17:19 +0000

    push a fe

---
+++ ch-7 Mathematical Library Methods/Section-B.3.java
@@ -0,0 +1,21 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        double radius, area, circumference;
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("Enter radius of a circle");
+        radius=sc.nextDouble();
+        System.out.println("radius of circle=\t"+radius);
+        //area of circlegit
+        area=(22/7)*Math.pow(radius,2);
+        System.out.println("area of circle is=\t"+area);
+        
+        //circumfarence
+        circumference=2*(22/7)*radius;
+        System.out.println("Circumfarence of a circle=\t"+circumference);
+        
+    }
+}
